52b809e2cc9500270ab224cde1fb02eed70a9966
meo84 <oopsiday@gmail.com>
2017-04-13 13:43:05 +0200

    fix indentation and query first table entry

--- filedb/lib/file_db/table.rb
+++ filedb/lib/file_db/table.rb
@@ -1,13 +1,15 @@
 module FileDb
   class Table
-  	attr_accessor :data, :name
+    attr_accessor :data, :name
     def initialize(data, name)
-    	@data = data
-    	@name = name
+      @data = data
+      @name = name
     end
 
-    def select
-      data[name]
+    def select(options={})
+      selection = data[name]
+      selection = data[name].select { |hash| hash["id"] == 1 } if options == {where: { id: 1 }}
+      return selection
     end
 
   end
--- filedb/test/file_db/table_test.rb
+++ filedb/test/file_db/table_test.rb
@@ -1,16 +1,20 @@
 require 'test_helper'
 
 class FileDb::TableTest < Minitest::Test
-	def setup
-    	@db = FileDb::Database.new('fixtures/data.json')
-    	@movies = @db.table("movies")
- 	end
+  def setup
+    @db = FileDb::Database.new('fixtures/data.json')
+    @movies = @db.table("movies")
+  end
 
- 	def test_select_method_returns_an_array
-    	assert_kind_of(Array, @movies.select)
-  	end
+  def test_select_method_returns_an_array
+    assert_kind_of(Array, @movies.select)
+  end
 
-  	def test_movies_select_returns_movies
-    	assert_equal(@db.data["movies"], @movies.select)
-  	end
+  def test_movies_select_returns_movies
+    assert_equal(@db.data["movies"], @movies.select)
+  end
+
+  def test_select_by_id_returns_movie
+    assert_equal([@db.data["movies"][0]], @movies.select(where: { id: 1 }))
+  end
 end
